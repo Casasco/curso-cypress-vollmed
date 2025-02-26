c1f63ef1e1d70bdff25d20f97c36e9e3e224aa25
Gabriel Casasco <gabriel.casasco@modalgr.io>
2025-02-26 14:23:50 -0300

    Segunda Aula

--- cypress/e2e/login-clinica.cy.js
+++ cypress/e2e/login-clinica.cy.js
@@ -3,8 +3,7 @@ describe('Página de Login', () => {
         cy.visit('')
         cy.contains('Entrar').click()
     })
-    it('Digita email e senha corretos para efetuar o login', { browser: 'firefox' }, () => {
+    it('Digita email e senha corretos para efetuar o login', { browser: 'edge' }, () => {
         cy.login(Cypress.env('email'), Cypress.env('senha'))
-
     })
 })
