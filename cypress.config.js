c5884c2b99576c1af92f940ec04d9c771c6bddcb
Gabriel Casasco <gabriel.casasco@modalgr.io>
2025-02-26 16:39:00 -0300

    Workflow cypress.yml

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env: {
-    "email": "catarina@email.com",
-    "senha": "Senha123",
-    "apiLogin": "http://localhost:8080/auth/login",
-    "apiClinica": "http://localhost:8080/clinica",
-    "apiEspecialista": "http://localhost:8080/especialista",
+    "email": "clinica@gmail.com",
+    "senha": "4321",
+    "api_login": "http://localhost:8080/auth/login",
+    "api_clinica": "http://localhost:8080/clinica",
+    "api_especialista": "http://localhost:8080/especialista",
     "requestMode": true
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners 
+      // implement node event listeners here
     },
     baseUrl: 'http://localhost:3000/',
     video: false,
@@ -23,8 +23,7 @@ module.exports = defineConfig({
       json: false,
       timestamp: "mmddyyyy_HHMMss"
     },
-    projectId: "xos7qy",
-
+    
     defaultCommandTimeout: 60000
   },
 });
